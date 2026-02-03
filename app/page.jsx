9d1b92cd5f68831ee57306cab3f9050e9cf0eb61
LittleBit <jaherron09@gmail.com>
2026-02-02 23:31:21 -0600

    More Prisma 7 fixes

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import '../styles/main.css';
-import { PrismaClient } from '@prisma/client';
 import PostContainer from './postcontainer';
 import ActiveProfileContainer from './activeprofilecontainer';
 
-export const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-})
+// prisma stuff
+import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+// For PostgreSQL using the pg driver:
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 export default async function HomePage() {
   return (
--- pages/api/profiles/[id].js
+++ pages/api/profiles/[id].js
@@ -1,8 +1,12 @@
 import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-})
+// For PostgreSQL using the pg driver:
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 export default async function handler(req, res) {
     const { id } = req.query;
